c59b346b233e389717e3fe7fde55d6bc83b98a55
Cameron Haygood <cameronhaygood@gmail.com>
2025-07-07 15:33:42 -0700

    minor update to add picture directory and user id to filepath

--- images.py
+++ images.py
@@ -4,12 +4,15 @@ from peewee import IntegrityError
 
 from socialnetwork_model import insert_table, search_table, update_table, delete_table, Pictures
 
+PICTURE_DIR = "pictures/"
+
 # Add Image to Pictures Table
 image_insert = insert_table(Pictures)
 
 def add_image(user_id, tags):
     '''Finds the last image ID in the Pictures table and increments it by 1'''
     image_id = find_next_image_id()
+    output_dir = convert_tags_to_dir(tags, user_id)
     image_data = {'picture_id':f"{image_id}", 'user_id': user_id, 'tags': tags}
     if image_insert(**image_data) is True:
         logger.info(f'Added {image_id} image to database')
@@ -29,11 +32,11 @@ def find_next_image_id():
             return next_unique_id
         counter += 1
 
-def convert_tags(tags):
+def convert_tags_to_dir(tags, user_id):
     tags = tags.replace('#', '').split()
     logger.debug(tags)
-    tags = tags.sort()
-    output_dir = "/".join(tags)
+    tags.sort()
+    output_dir = PICTURE_DIR+f"user_id/"+"/".join(tags)
     logger.debug(output_dir)
     return output_dir
 
