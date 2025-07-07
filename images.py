7a39fdc638dbb910f5c9a41c57c11405dbc10a63
Cameron Haygood <cameronhaygood@gmail.com>
2025-07-06 23:56:03 -0700

    Updated check to see if image id is valid

--- images.py
+++ images.py
@@ -9,15 +9,13 @@ image_insert = insert_table(Pictures)
 
 def add_image(user_id, tags):
     '''Finds the last image ID in the Pictures table and increments it by 1'''
-    image_counter = 2
-    try:
-        image_insert(picture_id=str(image_counter),
-                     user_id=user_id,
-                     tags=tags)
-        logger.info(f'Added {image_counter} image')
+    image_counter = 1
+    image_data = {'picture_id':f"{image_counter:010d}", 'user_id': user_id, 'tags': tags}
+    if image_insert(**image_data) is True:
+        logger.info(f'Added {image_counter} image to database')
         image_counter += 1
         return True
-    except IntegrityError:
+    else:
         logger.error(f'Integrity Error adding image: {image_counter}, {user_id}, {tags}')
         return False
 
